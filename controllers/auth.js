516d9431536b3e71b17b660ae008d6b6d987bdb4
Tommy-Lim <tlim24@gmail.com>
2017-01-09 12:52:12 -0800

    added auth controller and page icon

---
+++ controllers/auth.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var request = require('request');
+var db = require('../models');
+var router = express.Router();
+
+
+
+
+router.get('/signup', function(req, res){
+
+});
+
+router.post('/signup', function(req, res){
+
+});
+
+router.get('/login', function(req, res){
+
+});
+
+router.post('/login', function(req, res){
+
+});
+
+
+
+model.exports = router;
