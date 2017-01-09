efbb84cbe9e76c1c1cc33841f81d49d2f84fed10
Tommy-Lim <tlim24@gmail.com>
2017-01-09 11:31:49 -0800

    added details for each city

--- controllers/cities.js
+++ controllers/cities.js
@@ -22,29 +22,55 @@ router.post('/search', function(req, res){
 });
 
 router.get('/forecast/q/:id', function(req, res){
-  var query = req.params.id;
+  var query = req.params.id.split('=')[0];
+  var name = req.params.id.split('=')[1];
+
   query = "q/"+query;
   var url = "http://api.wunderground.com/api/b4b355346be47a17/forecast/"+query+".json";
 
   request.get(url, function(error, response, body){
     var weather = JSON.parse(body);
-    res.render('cities/forecast', {weather: weather});
+    res.render('cities/forecast', {
+      weather: weather,
+      name: name
+    });
   });
 
 });
 
 router.get('/historical/q/:id', function(req, res){
-  var query = req.params.id;
+  var query = req.params.id.split('=')[0];
+  var name = req.params.id.split('=')[1];
   query = "q/"+query;
 
-  //MMDD
+  //MMDD TODO: make this user input
   startDate = "0701";
   endDate = "0714";
   var url = "http://api.wunderground.com/api/b4b355346be47a17/planner_"+startDate+endDate+"/"+query+".json";
 
   request.get(url, function(error, response, body){
     var weather = JSON.parse(body);
-    res.render('cities/historical', {weather: weather});
+    res.render('cities/historical', {
+      weather: weather,
+      name: name
+    });
+  });
+
+});
+
+router.get('/details/q/:id', function(req, res){
+  var query = req.params.id.split('=')[0];
+  var name = req.params.id.split('=')[1];
+  query = "q/"+query;
+
+  var url = "http://api.wunderground.com/api/b4b355346be47a17/conditions/"+query+".json";
+
+  request.get(url, function(error, response, body){
+    var details = JSON.parse(body);
+    res.render('cities/details', {
+      details: details,
+      name: name
+    });
   });
 
 });
