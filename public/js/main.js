396598d18440f1560b0cafbf34bd651a8f93e95f
Tommy-Lim <tlim24@gmail.com>
2017-01-12 19:07:49 -0800

    added parallax to ejs layout

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,9 @@
-$(".back-button").on('click', function(){
-  window.history.back();
-});
-
 $(document).ready(function(){
   $('.carousel').carousel();
   $('.modal').modal();
   $(".button-collapse").sideNav();
+  $('.parallax').parallax();
+  $(".back-button").on('click', function(){
+    window.history.back();
+  });
 });
