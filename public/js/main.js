24066b4da9564bc51902a6c4a598e070ae35ae52
Tommy-Lim <tlim24@gmail.com>
2017-01-17 10:26:48 -0800

    added notes to main.js functions

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,28 @@
 $(document).ready(function(){
+  // ALLOW MODALS TO WORK - MATERIALIZE
   $('.modal').modal();
 
+  // ALLOW SIDE NAV TO WORK - MATERIALIZE
   $(".button-collapse").sideNav();
 
+  // ALLOW PARALLAX TO WORK - MATERIALIZE
   $('.parallax').parallax();
 
+  // ALLOW TOOLTIPS TO WORK - MATERIALIZE
+  $('.tooltipped').tooltip({delay: 50});
+
+  // BACK BUTTON FROM SEARCH FUNCTIONALITY
   $(".back-button").on('click', function(){
     window.history.back();
   });
 
-  $('.tooltipped').tooltip({delay: 50});
-
+  // FOCUS ON THE SEARCH BAR WHEN ADD DESTINATION CLICKED
   $('.profile-search-icons').on('click', function(e){
     e.preventDefault();
     $("#search-label").focus();
   });
 
+  // DISABLES FORM SUBMISSION TO RUN IP AND LAT/LON REQUEST FROM CLIENT, THEN RUN SERVER RQUEST
   $('#signup-form').on('submit', function(e){
     e.preventDefault();
     console.log("signup form submitted");
